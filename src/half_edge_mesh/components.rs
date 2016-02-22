ae463442626fd6439e3e40306c80f7937c7d9873
Mark Hintz <markohintz@gmail.com>
2016-02-23 00:04:03 +0100

    Add implementation of VertAdjacentVertIterator and improve the implementation of the other iterator.
    Implements PartialEq for the other storage classes and adds a utility function as well

--- src/half_edge_mesh/components.rs
+++ src/half_edge_mesh/components.rs
@@ -14,6 +14,13 @@ pub type VertRcPtr = Rc<RefCell<Vert>>;
 pub type FacePtr = Weak<RefCell<Face>>;
 pub type FaceRcPtr = Rc<RefCell<Face>>;
 
+fn merge_upgrade<T>(weak_a: & Weak<T>, weak_b: & Weak<T>) -> Option<(Rc<T>, Rc<T>)> {
+  match (weak_a.upgrade(), weak_b.upgrade()) {
+    (Some(strong_a), Some(strong_b)) => Some((strong_a, strong_b)),
+    _ => None
+  }
+}
+
 pub struct Edge {
   pub next: EdgePtr,
   pub pair: EdgePtr,
@@ -37,7 +44,48 @@ pub struct Vert {
 }
 
 impl Vert {
+  pub fn adjacent_verts(& self) -> VertAdjacentVertIterator {
+    VertAdjacentVertIterator {
+      start: self.edge.clone(),
+      current: None,
+    }
+  }
+}
 
+impl PartialEq<Vert> for Vert {
+  fn eq(& self, other: & Vert) -> bool { self.id == other.id }
+}
+
+pub struct VertAdjacentVertIterator {
+  start: EdgePtr,
+  current: Option<EdgePtr>,
+}
+
+impl Iterator for VertAdjacentVertIterator {
+  type Item = VertPtr;
+
+  // edge.pair.origin, edge = edge.pair.next, edge != start
+  fn next(&mut self) -> Option<VertPtr> {
+    return self.current.clone()
+      .and_then(|cur_weak: EdgePtr| cur_weak.upgrade())
+      .and_then(|cur_rc: EdgeRcPtr| cur_rc.borrow().pair.upgrade())
+      .and_then(|pair_rc: EdgeRcPtr| {
+        let next_weak: EdgePtr = pair_rc.borrow().next.clone();
+        return merge_upgrade(& next_weak, & self.start)
+          .and_then(|(next_rc, start_rc)| {
+            if next_rc != start_rc {
+              self.current = Some(next_weak);
+              Some(pair_rc.borrow().origin.clone())
+            } else { None }
+          });
+      })
+      .or_else(|| {
+        self.current = Some(self.start.clone());
+        return self.start.upgrade()
+          .and_then(|cur_rc: EdgeRcPtr| cur_rc.borrow().pair.upgrade())
+          .map(|pair_rc: EdgeRcPtr| pair_rc.borrow().origin.clone());
+      })
+  }
 }
 
 pub struct Face {
@@ -56,6 +104,10 @@ impl Face {
   }
 }
 
+impl PartialEq<Face> for Face {
+  fn eq(& self, other: & Face) -> bool { self.id == other.id }
+}
+
 pub struct FaceAdjacentVertIterator {
   start: EdgePtr,
   current: Option<EdgePtr>,
@@ -64,59 +116,46 @@ pub struct FaceAdjacentVertIterator {
 impl Iterator for FaceAdjacentVertIterator {
   type Item = VertPtr;
 
+  // edge.origin, edge = edge.next, edge != start
   fn next(&mut self) -> Option<VertPtr> {
-
     // map: Option<T>, Function<T -> U> -> Option<U>
-    // and_then: Option<T>, Function<T -> Option<U> -> Option<U>
-    // I think this is an equivalent calculation: 
+    // and_then: Option<T>, Function<T -> Option<U>> -> Option<U>
+    return self.current.clone()
+      .and_then(|cur_weak: EdgePtr| cur_weak.upgrade())
+      .map(|cur_rc: EdgeRcPtr| cur_rc.borrow().next.clone())
+      .and_then(|next_weak: EdgePtr| {
+        return merge_upgrade(& next_weak, & self.start)
+          .and_then(|(next_rc, start_rc)| {
+            if next_rc != start_rc {
+              self.current = Some(next_weak);
+              Some(next_rc.borrow().origin.clone())
+            } else { None }            
+          });
+      })
+      .or_else(|| {
+        return self.start.upgrade()
+          .map(|cur_rc: EdgeRcPtr| {
+            self.current = Some(self.start.clone());
+            cur_rc.borrow().origin.clone()
+          });
+      });
 
-    // self.current.clone()
-    //   .and_then(|cur_weak: EdgePtr| cur_weak.upgrade())
-    //   .map(|cur_rc: EdgeRcPtr| cur_rc.borrow().next.clone())
-    //   .and_then(|next_weak: EdgePtr| {
-    //     if let (Some(next_strong), Some(start_strong)) = (next_weak.upgrade(), self.start.upgrade()) {
-    //       if next_strong != start_strong {
-    //         self.current = Some(next_weak);
-    //         Some(next_strong.borrow().origin.clone())
+    // This should be an equivalent calculation to the above:
+    // if let Some(cur_weak) = self.current.clone() {
+    //   if let Some(cur_rc) = cur_weak.upgrade() {
+    //     let new_weak: EdgePtr = cur_rc.borrow().next.clone();
+    //     if let (Some(next_rc), Some(start_rc)) = (new_weak.upgrade(), self.start.upgrade()) {
+    //       if next_rc != start_rc {
+    //         self.current = Some(new_weak);
+    //         Some(next_rc.borrow().origin.clone())
     //       } else { None }
     //     } else { None }
-    //   })
-    //   .or_else(|| {
+    //   } else { None }
+    // } else {
+    //   if let Some(start_rc) = self.start.upgrade() {
     //     self.current = Some(self.start.clone());
-    //     self.start.upgrade()
-    //       .map(|cur_rc: EdgeRcPtr| cur_rc.borrow().origin.clone())
-    //   })
-
-    if let Some(cur_weak) = self.current.clone() {
-      if let Some(cur_rc) = cur_weak.upgrade() {
-        let new_weak: EdgePtr = cur_rc.borrow().next.clone();
-        if let (Some(new_strong), Some(start_strong)) = (new_weak.upgrade(), self.start.upgrade()) {
-          if new_strong != start_strong {
-            self.current = Some(new_weak);
-            Some(new_strong.borrow().origin.clone())
-          } else { None }
-        } else { None }
-      } else { None }
-    } else {
-      if let Some(start_strong) = self.start.upgrade() {
-        self.current = Some(self.start.clone());
-        Some(start_strong.borrow().origin.clone())
-      } else { None }
-    }
-
+    //     Some(start_rc.borrow().origin.clone())
+    //   } else { None }
+    // }
   }
-
 }
-
-// pub struct VertAdjacentVertIterator {
-//   start: EdgePtr,
-//   current: EdgePtr,
-// }
-
-// impl  Iterator for VertAdjacentVertIterator {
-//   type Item = Vert;
-
-//   fn next(&mut self) -> Option<Vert> {
-//     unimplemented!();
-//   }
-// }

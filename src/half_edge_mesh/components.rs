6efbf80df4b903c1e25e685dba3cf72f886cc9dc
Mark Hintz <markohintz@gmail.com>
2016-02-22 19:39:14 +0100

    An effort at fixing the lifetime issues that didn't work

--- src/half_edge_mesh/components.rs
+++ src/half_edge_mesh/components.rs
@@ -52,7 +52,8 @@ impl Face {
     FaceAdjacentVertIterator {
       start: self.edge.clone(),
       current: None,
-      phantom: PhantomData,
+      vert: None,
+      phantom: PhantomData
     }
   }
 }
@@ -60,6 +61,7 @@ impl Face {
 pub struct FaceAdjacentVertIterator<'a> {
   start: EdgeRef,
   current: Option<EdgeRef>,
+  vert: Option<VertRcRef>,
   phantom: PhantomData<Ref<'a, Vert>>
 }
 
@@ -71,12 +73,13 @@ impl<'a> Iterator for FaceAdjacentVertIterator<'a> {
     // map: T -> U
     // and_then: T -> Option<U>
     // I think this is an equivalent calculation: 
+
     // self.current
     //   .and_then(|cur_weak| cur_weak.upgrade())
     //   .and_then(|cur_ref| {
     //     let new_weak: EdgeRef = cur_ref.borrow().next.clone();
     //     new_weak.upgrade()
-    //       .and_then(|new_strong| {
+    //       .and_then(|new_strong: Rc<RefCell<Edge>>| {
     //         if new_strong != self.start {
     //           self.current = Some(new_weak.clone());
     //           new_strong.borrow().origin.upgrade()
@@ -97,20 +100,19 @@ impl<'a> Iterator for FaceAdjacentVertIterator<'a> {
         if let (Some(new_strong), Some(start_strong)) = (new_weak.upgrade(), self.start.upgrade()) {
           if new_strong != start_strong {
             self.current = Some(new_weak.clone());
-            if let Some(vert_ref) = new_strong.borrow().origin.upgrade() {
-              Some(vert_ref.borrow())
-            } else { None }
+            self.vert = new_strong.borrow().origin.upgrade();
+            self.vert.map(|v| Ref::clone(& v.borrow()))
           } else { None }
         } else { None }
       } else { None }
     } else {
-      self.current = Some(self.start.clone());
       if let Some(start_ref) = self.start.upgrade() {
-        if let Some(vert_ref) = start_ref.borrow().origin.upgrade() {
-          Some(vert_ref.borrow())
-        } else { None }
+        self.current = Some(self.start.clone());
+        self.vert = start_ref.borrow().origin.upgrade();
+        self.vert.map(|v| Ref::clone(& v.borrow()))
       } else { None }
     }
+
   }
 
 }

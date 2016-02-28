979d027442a34a3e2bb2a7b64711610cbc3d1f24
Mark Hintz <markohintz@gmail.com>
2016-02-28 21:41:24 +0100

    Implement connect_pairs

--- src/half_edge_mesh/mesh.rs
+++ src/half_edge_mesh/mesh.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use defs::*;
 
 use half_edge_mesh::components::{Edge, Vert, Face};
@@ -45,10 +47,69 @@ pub fn make_triangle(p1: & VertRc, p2: & VertRc, p3: & VertRc) -> (FaceRc, EdgeR
   (f1, e1, e2, e3)
 }
 
+fn merge_tuple_opt<A, B>(o: (Option<A>, Option<B>)) -> Option<(A, B)> {
+  match o {
+    (Some(a), Some(b)) => Some((a, b)),
+    _ => None
+  }
+}
+
+fn vert_ab_key(e: & EdgeRc) -> Option<(u32, u32)> {
+  let id_origin = e.borrow().origin.upgrade().map(|o| o.borrow().id);
+  let id_next_origin = e.borrow().next.upgrade().and_then(|n| n.borrow().origin.upgrade()).map(|o| o.borrow().id);
+  merge_tuple_opt((id_origin, id_next_origin))
+}
+
+fn vert_ba_key(e: & EdgeRc) -> Option<(u32, u32)> { vert_ab_key(e).map(|tuple| (tuple.1, tuple.0)) }
+
 // Takes what is assumed to be a fully-connected mesh, with no
 // pair links, and establishes pair links between adjacent edges
-pub fn connect_pairs(mesh: &mut Mesh) {
-  unimplemented!();
+pub fn connect_pairs(mesh: &mut Mesh) -> Result<(), &'static str> {
+  // Two-stage algorithm: first collect all edge A -> B relationships,
+  // Then go through and look for edges that are B -> A
+  let mut edge_hash: HashMap<(u32, u32), & EdgeRc> = HashMap::new();
+
+  for ref edge in mesh.edges.iter() {
+    // The types returned by match arms must be the same,
+    // hence the braces and semicolon used in the first branch
+    match vert_ab_key(edge) {
+      Some(key) => { edge_hash.insert(key, edge); },
+      // This happens if one of the mesh edges doesn't have a valid .origin or .next.origin pointer
+      None => { return Err("Could not hash all mesh edges"); }
+    }
+  }
+
+  for ref edge in mesh.edges.iter() {
+    // This if statement should skip half the edges, because two
+    // edge pairs are set each time it's true
+    if !edge.borrow().pair.is_valid() {
+      if let Some(key) = vert_ba_key(edge) {
+        match edge_hash.get(& key) {
+          Some(pair_edge) => {
+            // if one edge A -> B matches another edge B -> A, the edges are adjacent
+            edge.borrow_mut().take_pair(Ptr::new(pair_edge));
+            pair_edge.borrow_mut().take_pair(Ptr::new(edge));
+          },
+          None => { /* Happens when mesh is not closed */
+            return Err("Could not find pair edge");
+          }
+        }
+      } else {
+        // Theoretically this shouldn't ever happen
+        // because of the early return in the previous match block
+        return Err("Could not find reverse hash for mesh edge");
+      }
+    }
+  }
+
+  return Ok(());
+}
+
+fn report_connect_err(res: Result<(), &str>) {
+  match res {
+    Err(e) => println!("Error connecting mesh pairs! Mesh is not valid! {}", e),
+    _ => {},
+  }
 }
 
 impl Mesh {
@@ -74,7 +135,7 @@ impl Mesh {
 
     mesh.move_verts(vec![v1, v2, v3, v4]);
 
-    connect_pairs(&mut mesh);
+    report_connect_err(connect_pairs(&mut mesh));
 
     return mesh;
   }
@@ -101,7 +162,7 @@ impl Mesh {
 
     mesh.move_verts(vec![v1, v2, v3, v4, v5, v6]);
 
-    connect_pairs(&mut mesh);
+    report_connect_err(connect_pairs(&mut mesh));
 
     return mesh;
   }

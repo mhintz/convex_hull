f5b608f37f1adc5ad6fa2d05e6dfae3ad647d3e1
Mark Hintz <markohintz@gmail.com>
2016-02-28 22:16:22 +0100

    export pointer types

--- src/half_edge_mesh/mod.rs
+++ src/half_edge_mesh/mod.rs
@@ -7,3 +7,6 @@ pub use self::mesh::HalfEdgeMesh;
 pub use self::components::Edge;
 pub use self::components::Vert;
 pub use self::components::Face;
+
+// Export the pointer types too, in case you need them
+pub use self::ptr::*;

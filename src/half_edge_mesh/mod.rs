77ef9c1b0b82980da05f7a226867eb6de8a20909
Mark Hintz <markohintz@gmail.com>
2016-03-02 18:18:58 +0100

    Rename half_edge_mesh/mesh.rs to avoid confusion

--- src/half_edge_mesh/mod.rs
+++ src/half_edge_mesh/mod.rs
@@ -1,9 +1,9 @@
 pub mod ptr;
 pub mod components;
 pub mod iterators;
-pub mod mesh;
+pub mod half_edge_mesh;
 
-pub use self::mesh::HalfEdgeMesh;
+pub use self::half_edge_mesh::HalfEdgeMesh;
 pub use self::components::Edge;
 pub use self::components::Vert;
 pub use self::components::Face;

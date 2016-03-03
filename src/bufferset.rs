ca8d5ccc90feea829bc51e5d33bbfc0e44794afb
Mark Hintz <markohintz@gmail.com>
2016-03-03 01:55:56 +0100

    Implement from_half_edge_mesh_shared_verts

--- src/bufferset.rs
+++ src/bufferset.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use glium::backend::Facade;
 use glium::index::{PrimitiveType, IndexBuffer};
 use glium::vertex::VertexBuffer;
@@ -23,6 +25,13 @@ impl Vert {
     Vert {
       a_pos: pos.clone(),
       a_norm: norm.clone(),
+    }
+  }
+
+  pub fn pos_only(pos: [f32; 3]) -> Vert {
+    Vert {
+      a_pos: pos,
+      a_norm: [0.0; 3],
     }
   }
 }
@@ -85,6 +94,46 @@ impl BufferSet {
 
   pub fn from_half_edge_mesh_shared_verts <T> (gl: & T, mesh: & HalfEdgeMesh) -> BufferSet
   where T: Facade {
-    unimplemented!();
+    let mut v_buffer: Vec<Vert> = Vec::with_capacity(mesh.vertices.len());
+    let mut index_map: HashMap<u32, usize> = HashMap::new();
+
+    for (i, vert) in mesh.vertices.values().enumerate() {
+      v_buffer.push(Vert::pos_only(vert.borrow().pos.clone().into()));
+      index_map.insert(vert.borrow().id, i); // Gotta convert i: usize to i: u32
+    }
+
+    let mut i_buffer: Vec<u32> = Vec::with_capacity(mesh.faces.len() * 3);
+    let mut normal_counts: Vec<f32> = Vec::new();
+    normal_counts.resize(v_buffer.len(), 0.0);
+
+    for face in mesh.faces.values() {
+      face.borrow_mut().compute_attrs();
+      let face_normal = face.borrow().normal.clone();
+      for vert in face.borrow().adjacent_verts().to_ptr_vec() {
+        match index_map.get(& vert.borrow().id) {
+          Some(& idx) => {
+            i_buffer.push(idx as u32);
+            normal_counts[idx] += 1.0;
+            let a_norm = &mut v_buffer[idx].a_norm;
+            a_norm[0] += face_normal.x;
+            a_norm[1] += face_normal.y;
+            a_norm[2] += face_normal.z;
+          },
+          None => (),
+        }
+      }
+    }
+
+    for (idx, norm_count) in normal_counts.into_iter().enumerate() {
+      let a_norm = &mut v_buffer[idx].a_norm;
+      a_norm[0] /= norm_count;
+      a_norm[1] /= norm_count;
+      a_norm[2] /= norm_count;
+    }
+
+    BufferSet {
+      indices: IndexBuffer::new(gl, PrimitiveType::TrianglesList, & i_buffer[..]).unwrap(),
+      vertices: VertexBuffer::new(gl, & v_buffer[..]).unwrap(),
+    }
   }
 }

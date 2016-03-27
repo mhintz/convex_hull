1bb818fb0b5254a1e4cbd6ee9c0b2a5d57dcb086
Mark Hintz <markohintz@gmail.com>
2016-03-27 15:56:42 +0200

    Move half edge mesh dependency out into independent repo
    It's now hosted on crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@
 extern crate glium;
 extern crate cgmath;
 extern crate itertools;
+extern crate half_edge_mesh;
 
 pub mod bufferset;
 pub mod defs;
-pub mod half_edge_mesh;
 pub mod mesh;
 pub mod convex_hull;
 pub use convex_hull::get_convex_hull;
